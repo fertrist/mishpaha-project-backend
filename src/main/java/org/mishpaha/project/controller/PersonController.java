5dc299ca7b52abecc5baee01b9e967e83aa8f61c
pisaychuk <pisaychuk@hortonworks.com>
2015-11-16 17:20:15 +0200

    writing mock test

--- src/main/java/org/mishpaha/project/controller/PersonController.java
+++ src/main/java/org/mishpaha/project/controller/PersonController.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonView;
 import org.mishpaha.project.data.model.Person;
 import org.mishpaha.project.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -11,22 +13,25 @@ import java.util.List;
 /**
  * Created by fertrist on 29.10.15.
  */
-@RestController("/person")
+@RestController
 public class PersonController {
 
     @Autowired
     private PersonService personService;
 
+    @RequestMapping(value = "/summary/group/{id}", method = RequestMethod.GET)
     @JsonView(View.Summary.class)
     public List<Person> getHomeGroup(int groupId) {
         return personService.getHomeGroupList(groupId);
     }
 
+    @RequestMapping(value = "/summary/group/{id}")
     @JsonView(View.ExtendedSummary.class)
     public List<Person> getHomeGroupList(int groupId) {
         return personService.getHomeGroupList(groupId);
     }
 
+    @RequestMapping(value = "/info/group/{id}")
     @JsonView(View.FullInfo.class)
     public List<Person> getHomeGroupInfoList(int groupId) {
         return personService.getHomeGroupList(groupId);
--- src/main/java/org/mishpaha/project/test/TestPersonController.java
+++ src/main/java/org/mishpaha/project/test/TestPersonController.java
@@ -1,15 +1,39 @@
 package org.mishpaha.project.test;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mishpaha.project.config.MvcConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 /**
  * Created by fertrist on 12.11.15.
  */
-public class TestPersonController {
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {MvcConfiguration.class})
+@WebAppConfiguration
+public class TestPersonController extends BaseDaoTestClass {
+
+    @Autowired
+    protected WebApplicationContext wac;
+    private MockMvc mockMvc;
+
+    @BeforeClass
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
 
     @Test
-    public void listGroupRegionTribe() {
+    public void listGroupRegionTribe() throws Exception {
         //get list of group with all info
+        mockMvc.perform(MockMvcRequestBuilders.get(""));
 
         //get summary group info
 
