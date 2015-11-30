ff6ebc4048bb3c1542a0114364793f03cbdfbc18
pisaychuk <pisaychuk@hortonworks.com>
2015-11-30 14:44:28 +0200

    working group summary test

--- src/main/java/org/mishpaha/project/test/PersonControllerTest.java
+++ src/main/java/org/mishpaha/project/test/PersonControllerTest.java
@@ -1,6 +1,5 @@
 package org.mishpaha.project.test;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,9 +8,6 @@ import org.mishpaha.project.controller.PersonController;
 import org.mishpaha.project.data.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -19,12 +15,11 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.nio.charset.Charset;
-import java.util.Arrays;
 import java.util.List;
 
 import static java.lang.String.format;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -35,45 +30,20 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
 @WebAppConfiguration
 public class PersonControllerTest extends BaseDaoTestClass {
 
-    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
-            MediaType.APPLICATION_JSON.getSubtype(),
-            Charset.forName("utf8"));
-
-    private HttpMessageConverter mappingJackson2HttpMessageConverter;
-
     @Autowired
     private WebApplicationContext webApplicationContext;
-
     private MockMvc mockMvc;
-
     @Autowired
     private PersonController personController;
-
-    @Autowired
-    void setConverters(HttpMessageConverter<?>[] converters) {
-
-        this.mappingJackson2HttpMessageConverter = Arrays.asList(converters).stream().filter(
-                hmc -> hmc instanceof MappingJackson2HttpMessageConverter).findAny().get();
-
-        Assert.assertNotNull("the JSON message converter must not be null",
-                this.mappingJackson2HttpMessageConverter);
-    }
 
     @Before
     public void setup() throws Exception {
         this.mockMvc = webAppContextSetup(webApplicationContext).build();
     }
 
-//
-//  @BeforeClass
-//    public void setup() {
-//        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
-//    }
-
     @Test
     public void listGroupRegionTribe() throws Exception {
-        //get list of group with all info
-        //mockMvc = MockMvcBuilders.standaloneSetup(personController).build();
+        //get summary group info
         ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get("/summary/group/1"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("application/json;charset=UTF-8"))
@@ -82,10 +52,15 @@ public class PersonControllerTest extends BaseDaoTestClass {
         List<Person> personList = getPersonsByGroup(1);
 
         for (int i = 0; i < personList.size(); i++) {
-            resultActions.andExpect(jsonPath(format("$[%d].firstName", i), is(personList.get(i).getFirstName())));
+            //check that summary contains only few fields
+            resultActions.andExpect(jsonPath(format("$[%d].*", i), hasSize(4)))
+                .andExpect(jsonPath(format("$[%d].firstName", i), is(personList.get(i).getFirstName())))
+                .andExpect(jsonPath(format("$[%d].lastName", i), is(personList.get(i).getLastName())))
+                .andExpect(jsonPath(format("$[%d].midName", i), is(personList.get(i).getMidName())))
+                .andExpect(jsonPath(format("$[%d].categoryId", i), is(personList.get(i).getCategoryId())));
         }
 
-        //get summary group info
+
 
         //get region all info
 
