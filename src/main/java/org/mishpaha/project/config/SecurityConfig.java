a39de719e930f450c488dabd21f66f7221e5f80d
fertrist <fertrist@rambler.ru>
2016-05-02 13:31:59 +0300

    introduce error:  Refused to execute script from 'http://localhost:8080/js/hello.js' because its MIME type ('text/html') is not executable, and strict MIME type checking is enabled.

--- src/main/java/org/mishpaha/project/config/SecurityConfig.java
+++ src/main/java/org/mishpaha/project/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
@@ -13,7 +14,7 @@ import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 
 import javax.sql.DataSource;
 
-@Configuration
+@EnableWebSecurity
 @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
